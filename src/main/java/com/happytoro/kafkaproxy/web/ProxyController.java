24abc0eccc04c2e19141e9a13533aa693bf5338a
yeosiowvic <victoryeo1974@gmail.com>
2022-09-13 20:38:56 +0800

    add ResponseEntity

--- src/main/java/com/happytoro/kafkaproxy/web/ProxyController.java
+++ src/main/java/com/happytoro/kafkaproxy/web/ProxyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -31,17 +32,17 @@ public class ProxyController {
     public void context(ApplicationContext context) { this.context = context; }
 
     @GetMapping("/appname")
-    public String appname() {
+    public ResponseEntity<String> appname() {
         logger.info(appName);
-        return appName;
+        return ResponseEntity.status(HttpStatus.OK).body(appName);
     }
 
     @PostMapping("/order")
-    public String createOrder(@RequestBody Order order) {
+    public ResponseEntity<String> createOrder(@RequestBody Order order) {
         logger.info("createOrder");
         MessageProducer producer = this.context.getBean(MessageProducer.class);
         String orderStr = "Order " + order.getOrderType() + " " + order.getPrice() + " " + order.getQuantity();
 		producer.sendMessage(orderStr);
-        return "ok";
+        return ResponseEntity.status(HttpStatus.OK).body("ok");
     }
 }
