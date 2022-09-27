8db301398201aee439ffe085e59f8a2b354ea638
yeosiowvic <victoryeo1974@gmail.com>
2022-09-27 14:32:59 +0800

    re-do kafka consumer

--- src/main/java/com/happytoro/kafkaproxy/kafka/KafkaConsumer.java
+++ src/main/java/com/happytoro/kafkaproxy/kafka/KafkaConsumer.java
@@ -1,9 +1,9 @@
 package com.happytoro.kafkaproxy.kafka;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -12,33 +12,32 @@ import com.happytoro.kafkaproxy.firebase.FirebaseMessagingService;
 import com.happytoro.kafkaproxy.database.PriceService;
 import com.happytoro.kafkaproxy.model.Price;
 
-@Service
+@Component
 public class KafkaConsumer {
-
-    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
-
-    @Autowired private PriceService priceService;
+  private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
   
-    @Autowired
-    private FirebaseMessagingService firebaseService;
+  @Autowired private PriceService priceService;
+  
+  @Autowired
+  private FirebaseMessagingService firebaseService;
 
-    @Value(value = "${fcm.device.token}")
-    private String deviceToken;
+  @Value(value = "${fcm.device.token}")
+  private String deviceToken;
 
-    public void sendPushMessage(String title, String msg) throws FirebaseMessagingException {
-        firebaseService.sendNotification(title, msg, deviceToken);
-    }
+  public void sendPushMessage(String title, String msg) throws FirebaseMessagingException {
+      firebaseService.sendNotification(title, msg, deviceToken);
+  }
 
-    @KafkaListener(topics = "#{'${message.topic.consumer_name}'}",id = "myGroup")
-    public void consume(String message) throws Exception{ 
-        logger.info(String.format("Trade received: %s ", message ));
+	@KafkaListener(topics = "#{'${message.topic.consumer_name}'}", groupId = "group_id")
+	public void consume(String message) throws Exception {
+    logger.info(String.format("Trade received: %s ", message ));
 
-        String[] pricearray = message.split(" ");
-        String timestampTrade = pricearray[5] + " " + pricearray[6];
-        Price price = new Price(pricearray[1], pricearray[2], Float.parseFloat(pricearray[3]), timestampTrade);
-        System.out.println(price);
-        priceService.savePrice(price);
+    String[] pricearray = message.split(" ");
+    String timestampTrade = pricearray[5] + " " + pricearray[6];
+    Price price = new Price(pricearray[1], pricearray[2], Float.parseFloat(pricearray[3]), timestampTrade);
+    System.out.println(price);
+    priceService.savePrice(price);
 
-        sendPushMessage("Trade matched", message);
-    }
+    sendPushMessage("Trade matched", message);
+	}
 }
