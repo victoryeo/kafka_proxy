d843902c5bf406c4d3672df879bb1df959edeba7
Zi Hang <kevu.lee@gmail.com>
2022-09-28 10:58:10 +0800

    fixes for OIDs

--- src/main/java/com/happytoro/kafkaproxy/openOrders/service/OpenOrderService.java
+++ src/main/java/com/happytoro/kafkaproxy/openOrders/service/OpenOrderService.java
@@ -5,4 +5,5 @@ import com.happytoro.kafkaproxy.openOrders.model.OpenOrder;
 public interface OpenOrderService {
     OpenOrder saveOpenOrder(OpenOrder order);
     void updateOpenOrder(String makerOrderID, String takerOrderID, float amount);
+    OpenOrder getOpenOrder(String orderId);
 }
