a58cb3bbe084a3f1260480ce52fa27f8c3d9cc55
salikzquidz <salikinbaik@gmail.com>
2022-10-06 09:45:17 +0800

    remove accessToken field

--- src/main/java/com/happytoro/kafkaproxy/model/Order.java
+++ src/main/java/com/happytoro/kafkaproxy/model/Order.java
@@ -4,11 +4,10 @@ import lombok.Data;
 
 @Data
 public class Order {
-  private String orderID; 
+  private String orderID;
   private String tokenType;
   private String tokenName;
   private int orderType;
   private Double price;
   private Double quantity;
-  private String accessToken;
 }
