720e04c319384ebae2595f88f784d56b1acf43e2
yeosiowvic <victoryeo1974@gmail.com>
2022-10-11 14:36:18 +0800

    add test_ProxyController_order_null_access_token test case

--- src/test/java/com/happytoro/kafkaproxy/ProxyContollerTests.java
+++ src/test/java/com/happytoro/kafkaproxy/ProxyContollerTests.java
@@ -8,12 +8,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import org.springframework.http.MediaType;
 import static org.hamcrest.CoreMatchers.is;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.test.web.servlet.MvcResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.happytoro.kafkaproxy.web.ProxyController;
 
@@ -48,5 +53,38 @@ public class ProxyContollerTests {
       .contentType(MediaType.APPLICATION_JSON))
       .andExpect(status().isOk())
       .andExpect(jsonPath("$", is("kafkaproxy")));
+	}
+
+	@Test
+	public void test_ProxyController_order_null_access_token() throws Exception {
+
+		Map<String,Object> body = new HashMap<>();
+		body.put("orderID","1");
+		body.put("tokenType","stock");
+		body.put("tokenName","AAPL");
+		body.put("orderType", 0);
+		body.put("price","2");
+		body.put("quantity","2");
+
+		ObjectMapper mapper = new ObjectMapper();
+		//mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+		String requestJson=ow.writeValueAsString(body);
+
+		MvcResult result = mockMvc.perform(
+			post("/api/order")
+		 .contentType(MediaType.APPLICATION_JSON)
+		 .content(requestJson))
+		 .andReturn();
+		String contentP = result.getResponse().getContentAsString();
+		System.out.println("contentP "+contentP);
+
+		mockMvc.perform(
+			post("/api/order")
+		 .contentType(MediaType.APPLICATION_JSON)
+		 .content(requestJson))
+		 .andExpect(status().isBadRequest())
+		 .andExpect(jsonPath("$", is("Access token is null")));
+
 	}
 }
