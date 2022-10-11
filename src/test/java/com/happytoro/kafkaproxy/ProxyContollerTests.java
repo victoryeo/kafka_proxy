1c0a3d7ea4278f078cafc1a8449b80faecc5d673
yeosiowvic <victoryeo1974@gmail.com>
2022-10-11 14:00:34 +0800

    remove unused import

--- src/test/java/com/happytoro/kafkaproxy/ProxyContollerTests.java
+++ src/test/java/com/happytoro/kafkaproxy/ProxyContollerTests.java
@@ -7,16 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.http.MediaType;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.CoreMatchers.is;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.happytoro.kafkaproxy.web.ProxyController;
