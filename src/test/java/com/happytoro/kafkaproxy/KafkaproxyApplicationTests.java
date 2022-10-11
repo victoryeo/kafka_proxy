9c9a1634e72c2fffba8e00673d0fafd86b81efff
yeosiowvic <victoryeo1974@gmail.com>
2022-10-11 11:55:30 +0800

    add unit test

--- src/test/java/com/happytoro/kafkaproxy/KafkaproxyApplicationTests.java
+++ src/test/java/com/happytoro/kafkaproxy/KafkaproxyApplicationTests.java
@@ -1,13 +1,28 @@
 package com.happytoro.kafkaproxy;
 
+import org.junit.runner.RunWith;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.TestPropertySource;
+//import org.springframework.test.context.ContextConfiguration;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(classes=KafkaproxyApplicationTests.class)
+@RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+//@ContextConfiguration(locations = "classpath:test-app-context.xml")
 class KafkaproxyApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
 
+	@Value("${testvalue}")
+	String testvalue;
+
+	@Test
+	void test(){
+		assertThat(testvalue).isEqualTo("unittest");
+	}
 }
