f7a9a18d84f16f447fdb3d5242534e2d5b0f00b0
victoryeo <victoryeo1974@gmail.com>
2025-09-11 21:43:09 +0800

    change setting to build war package

--- src/main/java/com/happytoro/kafkaproxy/Application.java
+++ src/main/java/com/happytoro/kafkaproxy/Application.java
@@ -3,18 +3,22 @@ package com.happytoro.kafkaproxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer { // 1. Extend the initializer
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		System.out.println("Kafka Proxy running");
-
-		/* MessageProducer producer = context.getBean(MessageProducer.class);
-		producer.sendMessage("Hello, World!");
-
-		// shutdown
-		context.close();*/
+	@Override // 2. Override the configure method
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    // 3. Tell Spring Boot which is your primary application class
+    return application.sources(Application.class);
   }
+
+	// 4. Keep the main method for optional standalone JAR execution
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Kafka Proxy running");
+	}
+
 }
